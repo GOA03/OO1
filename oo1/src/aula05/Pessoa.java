87b4b6d1d87cfdd6a90a0265c3162791c0b7f8a9
GOA03 <91618139+GOA03@users.noreply.github.com>
2024-04-02 22:38:11 -0300

    Add files via upload

--- oo1/src/aula05/Pessoa.java
+++ oo1/src/aula05/Pessoa.java
@@ -2,33 +2,26 @@ package aula05;
 
 public class Pessoa {
 	
+	public Pessoa(String nome, int idade, double peso) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.peso = peso;
+	}
+
 	String nome;
 	int idade;
 	double peso;
 	
 	public void andar() {
-		System.out.println(nome + " está andando!");
+		
 	}
 	
 	public void correr() {
-		System.out.println(nome + " está correndo!");
+		
 	}
 	
 	public void falar() {
-		System.out.println(nome + " está falando!");
-	}
-	
-	public void imprimirDados() {
-		System.out.println("\n= - = DADOS = - =");
-		System.out.println("Nome: " + nome);
-		System.out.println("Idade: " + idade + "anos");
-		System.out.println("Peso: " + peso + "kg\n");
-	}
-	
-	public String toString() {
-		return "Nome: " + nome + 
-		" " + "\nIdade: " + idade +
-		" " + "\nPeso: " + peso +
-		"\n";
+		
 	}
 }
--- oo1/src/aula05/PessoaTeste.java
+++ oo1/src/aula05/PessoaTeste.java
@@ -4,28 +4,25 @@ public class PessoaTeste {
 	
 	public static void main(String[] args) {
 		
-		Pessoa pessoa1 = new Pessoa();
-		Pessoa pessoa2 = new Pessoa();
-		Pessoa pessoa3 = new Pessoa();
+		Pessoa pessoa1 = new Pessoa("João", 25, 68.5);
+		Pessoa pessoa2 = new Pessoa("Maria", 32, 55.6);
+		Pessoa pessoa3 = new Pessoa("José", 32, 62.4);
 		
-		pessoa1.nome = "João";
-		pessoa1.idade = 25;
-		pessoa1.peso = 68.5;
+		System.out.println("= = Pessoa 1 = = =");
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.idade);
+		System.out.println(pessoa1.peso + "\n");
 		
-		pessoa2.nome = "Maria";
-		pessoa2.idade = 30;
-		pessoa2.peso = 55.6;
 		
-		pessoa3.nome = "José";
-		pessoa3.idade = 32;
-		pessoa3.peso = 62.4;
+		System.out.println("= = Pessoa 2 = = =");
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.idade);
+		System.out.println(pessoa2.peso + "\n");
 		
-		System.out.println(pessoa2);
+		System.out.println("= = Pessoa 3 = = =");
+		System.out.println(pessoa3.nome);
+		System.out.println(pessoa3.idade);
+		System.out.println(pessoa3.peso);
 		
-		pessoa1.andar();
-		pessoa1.correr();
-		pessoa1.falar();
-		
-		pessoa1.imprimirDados();
 	}
 }
